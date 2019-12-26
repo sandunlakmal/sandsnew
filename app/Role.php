2bc266c6e9acbe74e62c1f49c499b4b730402f6f
sandun Lakmal <ysandun.lakmal@gmail.com>
2019-12-25 19:22:31 -0800

    Add AdminHome Controller

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    public function role(){
+
+
+
+        return $this->belongsTo('App\Role');
+        
+        }
 }
