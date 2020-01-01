d408f0c647f6a12a4abfd24abe6af1d91da455b7
sandun Lakmal <ysandun.lakmal@gmail.com>
2019-12-31 21:28:53 -0800

    last edite

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -9,5 +9,26 @@
     <h1>Create Users</h1>
 
 
+     {!! Form::open(['method'=>'POST', 'action'=> 'AdminUsersController@store','files'=>true]) !!}
 
-@stop
+
+      <div class="form-group">
+             {!! Form::label('name', 'Name:') !!}
+             {!! Form::text('name', null, ['class'=>'form-control'])!!}
+       </div>
+
+
+       <div class="form-group">
+        {!! Form::label('email', 'Email:') !!}
+        {!! Form::email('email', null, ['class'=>'form-control'])!!}
+       </div>
+
+        
+       {!! Form::close() !!}
+
+
+    @include('includes.form_error')
+
+
+
+ @stop
